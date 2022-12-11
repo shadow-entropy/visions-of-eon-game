d749b09120cb74d174ad1190e5ef70ccf4f0cc5c
shadow-entropy <180229@edu.psuti.ru>
2022-12-11 12:37:12 +0400

    Initial commit

---
+++ desktop/src/com/entropyzero/game/voe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.entropyzero.game.voe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.entropyzero.game.voe.VisionsOfEonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VisionsOfEonGame(), config);
+	}
+}
